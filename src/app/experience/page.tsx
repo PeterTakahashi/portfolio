23ce83d9f60219a9ef2909bce58836ece6a5a985
PeterTakahashi <twitter333555@gmail.com>
2025-08-22 13:48:01 +0900

    remove ex

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -19,23 +19,18 @@ function Appearance({
   title,
   description,
   event,
-  cta,
-  href,
 }: {
   title: string
   description: string
   event: string
-  cta: string
-  href: string
 }) {
   return (
     <Card as="article">
-      <Card.Title as="h3" href={href}>
+      <Card.Title as="h3">
         {title}
       </Card.Title>
       <Card.Eyebrow decorate>{event}</Card.Eyebrow>
       <Card.Description>{description}</Card.Description>
-      <Card.Cta>{cta}</Card.Cta>
     </Card>
   )
 }
@@ -53,43 +48,16 @@ export default function Experience() {
       intro="One of my favorite ways to share my ideas is live on stage, where there’s so much more communication bandwidth than there is in writing, and I love podcast interviews because they give me the opportunity to answer questions instead of just present my opinions."
     >
       <div className="space-y-20">
-        <ExperienceSection title="Conferences">
+        <ExperienceSection title="Experience">
           <Appearance
-            href="#"
             title="In space, no one can watch you stream — until now"
             description="A technical deep-dive into HelioStream, the real-time streaming library I wrote for transmitting live video back to Earth."
             event="SysConf 2021"
-            cta="Watch video"
           />
           <Appearance
-            href="#"
             title="Lessons learned from our first product recall"
             description="They say that if you’re not embarassed by your first version, you’re doing it wrong. Well when you’re selling DIY space shuttle kits it turns out it’s a bit more complicated."
             event="Business of Startups 2020"
-            cta="Watch video"
-          />
-        </ExperienceSection>
-        <ExperienceSection title="Podcasts">
-          <Appearance
-            href="#"
-            title="Using design as a competitive advantage"
-            description="How we used world-class visual design to attract a great team, win over customers, and get more press for Planetaria."
-            event="Encoding Design, July 2022"
-            cta="Listen to podcast"
-          />
-          <Appearance
-            href="#"
-            title="Bootstrapping an aerospace company to $17M ARR"
-            description="The story of how we built one of the most promising space startups in the world without taking any capital from investors."
-            event="The Escape Velocity Show, March 2022"
-            cta="Listen to podcast"
-          />
-          <Appearance
-            href="#"
-            title="Programming your company operating system"
-            description="On the importance of creating systems and processes for running your business so that everyone on the team knows how to make the right decision no matter the situation."
-            event="How They Experience Radio, September 2021"
-            cta="Listen to podcast"
           />
         </ExperienceSection>
       </div>
